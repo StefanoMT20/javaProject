e97f5deef4912ac42ecc25756cf657cb94228c61
Piero Tejada <userpiero@Pieros-Mac-mini.local>
2022-06-12 19:29:54 -0500

    initilializing project

---
+++ src/main/java/org/VirtualGames/ProyectoDawiVirtualGamesApplication.java
@@ -0,0 +1,13 @@
+package org.VirtualGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawiVirtualGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawiVirtualGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/VirtualGames/ProyectoDawiVirtualGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.VirtualGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawiVirtualGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
